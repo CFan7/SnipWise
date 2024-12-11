13daafddf58c0ae4041741f5c5c2dd3f5c38bfbb
Peilin Shen <shnpl@outlook.com>
2024-12-11 12:22:44 -0600

    GET /api/companies/snip8/groups

--- backend/src/main/java/com/snipwise/controller/GroupController.java
+++ backend/src/main/java/com/snipwise/controller/GroupController.java
@@ -2,15 +2,20 @@ package com.snipwise.controller;
 
 import com.snipwise.exception.ClientNotExistException;
 import com.snipwise.exception.ClientUnauthorizedException;
+import com.snipwise.pojo.CompanyGetGroupResponseDTO;
 import com.snipwise.pojo.Group;
 import com.snipwise.pojo.GroupCreateDTO;
 import com.snipwise.pojo.GroupCreateResponseDTO;
+import com.snipwise.service.CompanyService;
+import com.snipwise.service.CompanyServiceImpl;
 import com.snipwise.service.GroupService;
 import io.fusionauth.jwt.JWTException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -19,6 +24,9 @@ public class GroupController
 {
     @Autowired
     private GroupService groupService;
+    @Autowired
+    private CompanyService companyService;
+
     @PostMapping()
     public ResponseEntity<GroupCreateResponseDTO> createGroup(@RequestHeader("Authorization") String jwtString, @PathVariable String companyName, @RequestBody GroupCreateDTO groupCreateDTO)
     {
@@ -41,5 +49,27 @@ public class GroupController
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
 
+    }
+    @GetMapping()
+    public ResponseEntity<List<CompanyGetGroupResponseDTO>> getGroups(@RequestHeader("Authorization") String jwtString, @PathVariable String companyName)
+    {
+        try
+        {
+            List<CompanyGetGroupResponseDTO>  result = companyService.getCompanyGroups(jwtString,companyName);
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+        catch (JWTException | ClientUnauthorizedException e)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+        catch (ClientNotExistException e)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }
